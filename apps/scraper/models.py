5fc686f42a1cc1a878c5090a26f1dbe1de0e6581
Natnael Abrham <natnael08@protonmail.com>
2025-01-16 20:58:56 +0300

    feat: add Amazon Product model

--- apps/scraper/models.py
+++ apps/scraper/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    seller = models.CharField(max_length=255)
+    rating = models.DecimalField(max_digits=3, decimal_places=2)
+    review_count = models.PositiveIntegerField(default=0)
+    images = models.TextField()
+
+    def __init__(self, *args, **kwargs):
+        return self.title
